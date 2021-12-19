21723bc0e2a52ae50431691203bbd752d3ae301a
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2021-12-19 19:59:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,9 +21,10 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-jd-dailybonus
 #=================================================
 # 拉取
+git clone https://github.com/kenzok8/openwrt-packages/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.10/g' package/base-files/files/bin/config_generate
