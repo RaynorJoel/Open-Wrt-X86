91d10cc2dc970b989a5aa1f7f727f8105bfed562
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2021-12-19 14:29:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,6 @@
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-git clone https://github.com/sirpdboy/NetSpeedTest package/lean/NetSpeedTest
 #=================================================
 # 清除旧版
 # 删除
@@ -22,7 +21,7 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-jd-dailybonus
 #=================================================
 # 拉取
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon18.06
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 #=================================================
