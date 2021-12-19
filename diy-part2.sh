a6114a2b76eed4a473085ed167d0a9e131b00ac8
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2021-12-19 19:56:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-jd-dailybonus
 #=================================================
 # 拉取
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 #=================================================
