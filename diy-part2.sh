ee849fc318092f6b4eee2581bd572503053389db
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2021-12-20 10:27:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 #=================================================
 # 增加插件包
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 #=================================================
 # 清除旧版
@@ -21,8 +21,8 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-jd-dailybonus
 #=================================================
 # 拉取
-git clone https://github.com/RaynorJoel/luci-theme-argon.git package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# git clone https://github.com/RaynorJoel/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 #=================================================
