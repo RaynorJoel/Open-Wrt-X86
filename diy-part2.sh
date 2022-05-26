4e5cff4e1f0def3d10584f3af0ffce817988049d
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2022-05-26 18:43:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 #=================================================
 # 增加插件包
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 #=================================================
 # 清除旧版
 # 删除
