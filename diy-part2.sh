6a51fb2ddfdba20c1f29ee7b3ee7cf754f2284aa
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2022-05-27 01:33:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,8 @@ git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-w
 #=================================================
 # 清除旧版
 # 删除
-rm -rf package/lean/themes/luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-argon-config
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/kenzo/luci-app-argon-config
 #=================================================
 # 拉取
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/themes/luci-theme-argon
