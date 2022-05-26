fc3962efadb045a4b883974727888adb277e92f6
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2022-05-26 17:09:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-argon-config
 #=================================================
 # 拉取
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 #=================================================
 # Modify default IP
