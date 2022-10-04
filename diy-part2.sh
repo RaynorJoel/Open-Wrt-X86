89b4adbd57da24b77f4415e5245552eeb792c000
RaynorJoel <47142397+RaynorJoel@users.noreply.github.com>
2022-10-05 01:23:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,14 @@
 #=================================================
 # 增加插件包
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/firker/luci-app-wrtbwmon-zh.git package/lean/luci-app-wrtbwmon-zh
 #=================================================
 # 清除旧版
 # 删除
 # rm -rf package/feeds/luci/luci-theme-argon
-# rm -rf package/feeds/kenzo/luci-app-argon-config
+rm -rf package/feeds/kenzo/luci-app-argon-config
 #=================================================
 # 拉取
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/themes/luci-theme-argon
